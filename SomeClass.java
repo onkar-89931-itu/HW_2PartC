0bdc846e191913df55a5eb2acd41ac55878a4380
Onkar <89931.kale@students.itu.edu>
2016-04-24 17:31:37 -0700

    Refactored FnOne to thoughtForTheDay

--- SomeClass.java
+++ SomeClass.java
@@ -1,12 +1,12 @@
 public class SomeClass{
-publi void fnOne(){
+publi void thoughtForTheDay(){
 system.out.println("I could'nt repair your brakes, so I made your horn louder");
 
 }
 
 public static void main (String[]args){
 SomeClass sc = new SomeClass();
-sc.fnOne();
+sc.thoughtForTheDay();
 
 
 }
---
+++ SomeClass.java~
@@ -0,0 +1,13 @@
+public class SomeClass{
+publi void fnOne(){
+system.out.println("I could'nt repair your brakes, so I made your horn louder");
+
+}
+
+public static void main (String[]args){
+SomeClass sc = new SomeClass();
+sc.fnOne();
+
+
+}
+}
