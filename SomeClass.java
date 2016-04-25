96154a3a465bfcd5d2683787f75500783a3d2341
Onkar <89931.kale@students.itu.edu>
2016-04-24 18:07:19 -0700

    Added Events

--- SomeClass.java
+++ SomeClass.java
@@ -1,13 +1,14 @@
 public class SomeClass{
 publi void thoughtForTheDay(){
-system.out.println("On the other hand, you have different fingers");
-
+system.out.println("I could'nt repair your brakes, so I made yourgit horn louder");
+publi void upcomingEvents(){
+system.out.println("Upcomin Events");
+system.out.println("Party at my house tomorrow");
 }
-
+}
 public static void main (String[]args){
 SomeClass sc = new SomeClass();
 sc.thoughtForTheDay();
-
-
+sc.upcomingEvents();
 }
 }
